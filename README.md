650339545b640c15b28e9467cb6be58f95fbe627
Pob <590650@gmail.com>
2019-10-18 23:56:20 +0700

    add test for parseLinkHeader

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 # Note
 
 - breakpoint : 576px
-- new Date().toISOString()
 - https://api.github.com/search/repositories?q=created:>2019-10-13T00:00:01Z&sort=stars&order=desc&per_page=5&page=2
-- test 30 day with month that contain 30, 31 days
 - debounce while scrolling
-- repo name can be very long text
 - cypress dependencies for ubuntu `apt-get install xvfb libgtk-3-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2`
 - add readme to list all commands
 - deploy to netlify and change baseURL of e2e test
---
+++ src/utils/__tests__/parseLinkHeader.test.js
@@ -0,0 +1,33 @@
+import { parseLinkHeader } from '../parseLinkHeader'
+
+test('turns Link header in the response of requesting the first page into an object', () => {
+  const linkHeader =
+    '<https://api.mock.com/q?page=2>; rel="next", <https://api.mock.com/q?page=100>; rel="last"'
+  const result = parseLinkHeader(linkHeader)
+  expect(result).toEqual({
+    next: 'https://api.mock.com/q?page=2',
+    last: 'https://api.mock.com/q?page=100'
+  })
+})
+
+test('turns Link header in the response of requesting other pages into an object', () => {
+  const linkHeader =
+    '<https://api.mock.com/q?page=1>; rel="prev", <https://api.mock.com/q?page=3>; rel="next", <https://api.mock.com/q?page=100>; rel="last", <https://api.mock.com/q?page=1>; rel="first"'
+  const result = parseLinkHeader(linkHeader)
+  expect(result).toEqual({
+    prev: 'https://api.mock.com/q?page=1',
+    next: 'https://api.mock.com/q?page=3',
+    last: 'https://api.mock.com/q?page=100',
+    first: 'https://api.mock.com/q?page=1'
+  })
+})
+
+test('turns Link header in the response of requesting the last page into an object', () => {
+  const linkHeader =
+    '<https://api.mock.com/q?page=99>; rel="prev", <https://api.mock.com/q?page=1>; rel="first"'
+  const result = parseLinkHeader(linkHeader)
+  expect(result).toEqual({
+    prev: 'https://api.mock.com/q?page=99',
+    first: 'https://api.mock.com/q?page=1'
+  })
+})
